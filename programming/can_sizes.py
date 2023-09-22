02e177f3dd1e479377b4b13fad3b698be686492a
luisquispe <luisquispetello@gmail.com>
2023-09-22 19:10:44 +0200

    add new exercise

--- programming/can_sizes.py
+++ programming/can_sizes.py
@@ -1,23 +1,61 @@
 from math import pi
 
-def main():
-    return compute_storage_efficiency()
 
-def compute_volume(radius, height):
+def main():
+
+    with open('can_info.txt') as can_info:
+        next(can_info)
+
+        best_store = None
+        best_cost = None
+        max_store_eff = -1
+        max_cost_eff = -1
+
+        for data in can_info:
+            row = data.split('	')
+            name = row[0]
+            radius = float(row[1])
+            height = float(row[2])
+            cost = float(row[3].lstrip('$'))
+
+            volume = calculate_volume(radius, height)
+            surface_area = calculate_surface_area(radius, height)
+            storage_efficiency = compute_storage_efficiency(volume, surface_area)
+            cost_efficiency = compute_cost_efficiency(volume, cost)
+
+            print(f'{name} {storage_efficiency:.2f} {cost_efficiency:.2f} ')
+
+            if storage_efficiency > max_store_eff:
+                best_store = name
+                max_store_eff = storage_efficiency
+
+            if cost_efficiency > max_cost_eff:
+                best_cost = name
+                max_cost_eff = cost_efficiency
+
+        print()
+        print("Best can size in storage efficiency:", best_store)
+        print("Best can size in cost efficiency:", best_cost)
+
+
+def calculate_volume(radius, height):
     volume = pi * radius**2 * height
     return volume
 
-def compute_surface_area(radius, height):
-    area = 2 * pi * radius * (radius + height)
-    return area
 
-def compute_storage_efficiency():
-    volume = compute_volume()
-    area = compute_surface_area()
-    storage_efficiency = volume / area
+def calculate_surface_area(radius, height):
+    surface_area = 2 * pi * radius * (radius + height)
+    return surface_area
+
+
+def compute_storage_efficiency(volume, surface_area):
+    storage_efficiency = volume / surface_area
     return storage_efficiency
 
-def compute_cost_efficiency():
-    return 0
+
+def compute_cost_efficiency(volume, cost):
+    cost_efficiency = volume / cost
+    return cost_efficiency
+
 
 main()
--- programming/scene.py
+++ programming/scene.py
@@ -6,7 +6,6 @@ from draw2d import \
     start_drawing, draw_line, draw_oval, draw_arc, \
     draw_rectangle, draw_polygon, draw_text, finish_drawing
 
-
 def main():
     scene_width = 800
     scene_height = 500
@@ -18,7 +17,7 @@ def main():
     # Call the draw_sky and draw_ground functions in this file.
     draw_sky(canvas, scene_width, scene_height)
     draw_ground(canvas, scene_width, scene_height)
-    draw_grid(canvas, scene_width, scene_height, 50)
+    #draw_grid(canvas, scene_width, scene_height, 50)
 
     # Call the finish_drawing function
     # in the draw2d.py library.
@@ -28,75 +27,108 @@ def main():
 def draw_sky(canvas, scene_width, scene_height):
     """Draw the sky and all the objects in the sky."""
     draw_rectangle(canvas, 0, scene_height / 3,
-        scene_width, scene_height, width=0, fill="sky blue")
-    
-    draw_cloud(canvas, 100, 100, 300)
+        scene_width, scene_height, width=0, fill="skyBlue2")
+    draw_cloud(canvas)
 
 
 def draw_ground(canvas, scene_width, scene_height):
     """Draw the ground and all the objects on the ground."""
     draw_rectangle(canvas, 0, 0,
-        scene_width, scene_height / 3, width=0, fill="tan4")
+        scene_width, scene_height / 1.4, width=0, fill="skyBlue4")
+    
+    draw_rectangle(canvas, 0, 0, scene_width, scene_height / 2, width=1, outline="darkGreen", fill="darkGreen")
+    
+    draw_rectangle(canvas, 0, 0,
+        scene_width, scene_height / 2.5, width=0, fill="burlyWood3")
+    
+    draw_rectangle(canvas, 0, 0,
+        scene_width, scene_height / 5, width=0, fill="springGreen3")
+    draw_rectangle(canvas, 0, 0,
+        scene_width, scene_height / 8, width=0, fill="springGreen2")
+    
+    # Draw
+    draw_mountain(canvas)
+    draw_buildings(canvas)
+    draw_grass(canvas)
+    draw_stairs(canvas)
 
-    # Draw a pine tree.
-    tree_center_x = 170
-    tree_bottom = 100
-    tree_height = 200
-    draw_pine_tree(canvas, tree_center_x,
-            tree_bottom, tree_height)
 
-    # Draw another pine tree.
-    tree_center_x = 90
-    tree_bottom = 70
-    tree_height = 220
-    draw_pine_tree(canvas, tree_center_x,
-            tree_bottom, tree_height)
+def draw_cloud(canvas):
+    draw_oval(canvas, 750, 450, 500, 350, width=0, fill="snow1")
+    draw_oval(canvas, x0 = 400, y0 = 480, x1 = 100, y1 = 400,  width=0, fill="snow1")
+    draw_rectangle(canvas, 600, 450, 0, 380, width=0, fill="snow1")
+    draw_rectangle(canvas, 300, 400, 150, 350, width=0, fill="snow1")
+    draw_rectangle(canvas, 800, 460, 600, 400, width=0, fill="snow1")
+    draw_rectangle(canvas, 250, 480, 30, 460, width=0, fill="snow1")
+    draw_rectangle(canvas, 650, 360, 400, 350, width=0, fill="snow1")
 
 
+def draw_mountain(canvas):
+    #mountains background
+    draw_polygon(canvas, x0 = 150, y0 = 250, x1 = 350, y1 = 250, x2 = 250, y2 = 300, width=0, fill="skyBlue4")
+    draw_oval(canvas, 100, 480, -400, 200, width=0, fill="skyBlue4")
+    draw_oval(canvas, 900, 400, 750, 300, width=0, fill="skyBlue4")
+    draw_oval(canvas, 450, 380, 315, 300, width=0, fill="skyBlue4")
+
+    #big mountains
+    draw_polygon(canvas, x0 = 250, y0 = 250, x1 = 650, y1 = 250, x2 = 450, y2 = 480, width=0, outline="dark green", fill="dark green")
+    draw_polygon(canvas, x0 = 150, y0 = 250, x1 = 350, y1 = 250, x2 = 250, y2 = 300, width=0, outline="dark green", fill="dark green")
+    draw_rectangle(canvas, x0 = 700, y0 = 320, x1 = 550, y1 = 250, width=0, outline="dark green", fill="darkGreen")
+    draw_polygon(canvas, x0 = 700, y0 = 250, x1 = 800, y1 = 250, x2 = 700, y2 = 320, width=0, outline="dark green", fill="dark green")
+    draw_rectangle(canvas, x0 = 800, y0 = 270, x1 = 550, y1 = 250, width=0, outline="dark green", fill="darkGreen")
+
+    #cloud
+    draw_rectangle(canvas, 440, 450, 0, 380, width=0, fill="snow1")
+    
+    #shades of mountains
+    draw_polygon(canvas, 150, 250, 220, 250, 250, 300, width=0, outline="darkSlateGray", fill="darkSlateGray")
+    draw_polygon(canvas, 270, 290, 420, 200, 450, 480, width=0, outline="darkSlateGray", fill="darkSlateGray")
+    draw_polygon(canvas, 700, 230, 800, 230, 700, 300, width=0, outline="darkSlateGray", fill="darkSlateGray")
+    draw_rectangle(canvas, 700, 320, 600, 200, width=0, outline="darkSlateGray", fill="darkSlateGray")
 
 
+def draw_buildings(canvas):
+    draw_rectangle(canvas, 400, 220, 0, 200, fill="burlyWood3")
+    draw_rectangle(canvas, 800, 230, 550, 200, fill="burlyWood3")
+    draw_rectangle(canvas, 150, 290, 0, 200, fill="burlyWood3")
+    draw_rectangle(canvas, 100, 325, 0, 200, outline='burlyWood3', fill="burlyWood3")
+    draw_rectangle(canvas, 500, 280, 400, 200, outline='burlyWood3', fill="burlyWood3")
+    draw_rectangle(canvas, 750, 260, 650, 200, outline='burlyWood3', fill="burlyWood3")
+    draw_rectangle(canvas, 750, 270, 700, 200, outline='burlyWood3', fill="burlyWood3")
+    draw_polygon(canvas, 0, 300, 100, 300, 0, 350, width=0, outline='burlyWood3', fill="burlyWood3")
+    draw_rectangle(canvas, 320, 240, 0, 200, outline='burlyWood3', fill="burlyWood3")
+    draw_rectangle(canvas, 500, 290, 490, 280, outline='burlyWood3', fill="burlyWood3")
+    draw_rectangle(canvas, 560, 240, 550, 230, outline='burlyWood3', fill="burlyWood3")
 
-def draw_cloud(canvas, center_x, bottom, height):
-    x0 = center_x
-    y0 = center_x
-    x1 = bottom
-    y1 = height
-    draw_oval(canvas, x0, y0, x1, y1, width=1, outline='white', fill='grey')
+    #shadow
+    draw_rectangle(canvas, 100, 190, 50, 100, outline="saddleBrown", fill="saddleBrown")
+    draw_rectangle(canvas, 50, 240, 0, 100, outline="saddleBrown", fill="saddleBrown")
+    draw_rectangle(canvas, 610, 210, 580, 90, outline="saddleBrown", fill="saddleBrown")
 
-def draw_pine_tree(canvas, center_x, bottom, height):
-    """Draw a single pine tree.
-    Parameters
-        canvas: The canvas where this function
-            will draw a pine tree.
-        center_x, bottom: The x and y location in pixels where
-            this function will draw the bottom of a pine tree.
-        height: The height in pixels of the pine tree that
-            this function will draw.
-    Return: nothing
-    """
-    trunk_width = height / 10
-    trunk_height = height / 8
-    trunk_left = center_x - trunk_width / 2
-    trunk_right = center_x + trunk_width / 2
-    trunk_top = bottom + trunk_height
+    #windows
+    draw_rectangle(canvas, 480, 250, 470, 220, outline="burlyWood1", fill="orange3")
+    draw_rectangle(canvas, 430, 250, 420, 220, outline="burlyWood1", fill="orange3")
 
-    # Draw the trunk of the pine tree.
-    draw_rectangle(canvas,
-            trunk_left, trunk_top, trunk_right, bottom,
-            outline="gray20", width=1, fill="tan3")
+    draw_rectangle(canvas, 200, 220, 180, 200, outline="burlyWood1", fill="orange3")
+    draw_rectangle(canvas, 260, 220, 240, 200, outline="burlyWood1", fill="orange3")
 
-    skirt_width = height / 2
-    skirt_height = height - trunk_height
-    skirt_left = center_x - skirt_width / 2
-    skirt_right = center_x + skirt_width / 2
-    skirt_top = bottom + height
+    draw_rectangle(canvas, 740, 240, 730, 200, outline="orange3", fill="burlyWood1")
+    draw_rectangle(canvas, 700, 240, 690, 200, outline="orange3", fill="burlyWood1")
 
-    # Draw the crown (also called skirt) of the pine tree.
-    draw_polygon(canvas, center_x, skirt_top,
-            skirt_right, trunk_top,
-            skirt_left, trunk_top,
-            outline="gray20", width=1, fill="dark green")
 
+def draw_grass(canvas):
+    draw_polygon(canvas, 0, 100, 150, 100, 0, 200, outline='springGreen3', fill='springGreen3')
+    draw_polygon(canvas, 0, 250, 50, 250, 100, 290, outline='springGreen3', fill='springGreen3')
+    draw_polygon(canvas, 50, 200, 100, 200, 150, 290, outline='springGreen3', fill='springGreen3')
+    draw_rectangle(canvas, 350, 110, 250, 100, outline='springGreen3', fill='springGreen3')
+    draw_rectangle(canvas, 400, 60, 390, 50, outline='springGreen3', fill="burlyWood3")
+    draw_polygon(canvas, 0, 50, 150, 50, 0, 150, outline='springGreen2', fill="springGreen2")
+
+
+def draw_stairs(canvas):
+    draw_rectangle(canvas, 550, 200, 500, 75, outline="burlyWood", fill="burlyWood")
+    draw_rectangle(canvas, 550, 170, 500, 155, outline='burlyWood', fill="burlyWood3")
+    draw_rectangle(canvas, 550, 120, 500, 105, outline='burlyWood', fill="burlyWood3")
 
 
 
